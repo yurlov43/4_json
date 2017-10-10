48f48202aacaa2b5bdaabdda0d71f68e8a4092ca
yurlov43 <yurlov43@yandex.ru>
2017-10-10 19:09:38 +0300

    Скрнипт, печатающий json, готов

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,30 @@
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
+    data = ""
+    try:
+        with open(filepath, "r") as f:
+            for line in f:
+                data += line
+        data = json.loads(data)
+        pretty_print_json(data)
+    except IOError as e:
+        print("Ошибка. Файл не найден.")
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, sort_keys=True, indent=4))
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) < 2:
+        print("Ошибка. Не указан файл.")
+        sys.exit(1)
+
+    if len(sys.argv) > 2:
+        print("Ошибка. Слишком много параметров.")
+        sys.exit(1)
+    filepath = sys.argv[1]
+    load_data(filepath)
