1ec30bdc97ae3749c9d409da2c3957c208889180
yurlov43 <yurlov43@yandex.ru>
2017-10-11 17:21:14 +0300

    Доработана чистота функций, отделён ввод от обработки, исправлено кодирование кириллицы в выводе

--- pprint_json.py
+++ pprint_json.py
@@ -5,23 +5,29 @@ import sys
 def load_data(filepath):
     try:
         with open(filepath, "r") as file_json:
-            data_json = json.load(file_json)
-        pretty_print_json(data_json)
-    except IOError as error:
-        print("Ошибка. Файл не найден.")
+            str_json = file_json.read()
+        if not str_json:
+            raise sys.exit("Ошибка: файл пуст.")
+        return str_json
+    except IOError as err:
+        sys.exit("Ошибка: файл не найден.")
+
+
+def load_json(str_json):
+    return json.loads(str_json)
 
 
 def pretty_print_json(data_json):
-    print(json.dumps(data_json, sort_keys=True, indent=4))
+    print(json.dumps(data_json, ensure_ascii=False, sort_keys=True, indent=4))
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print("Ошибка. Не указан файл.")
-        sys.exit(1)
+        sys.exit("Ошибка: не указан файл.")
 
     if len(sys.argv) > 2:
-        print("Ошибка. Слишком много параметров.")
-        sys.exit(1)
+        sys.exit("Ошибка: слишком много параметров.")
     filepath = sys.argv[1]
-    load_data(filepath)
+    str_json = load_data(filepath)
+    data_json = load_json(str_json)
+    pretty_print_json(data_json)
